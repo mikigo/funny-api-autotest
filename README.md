53e5879077e60e03563c55f15a8f2689a781b8b3
mikigo <huangmingqiang@uniontech.com>
2023-07-03 15:35:22 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# funny-api-autotest
+# httrrunner-demo
+
---
+++ mock.py
@@ -0,0 +1,20 @@
+import os
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_item(name: str = ""):
+    return {"name": name}
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        app="mock:app",
+        host=os.popen("hostname -I").read().split(" ")[0],
+        port=5000,
+        reload=True
+    )
